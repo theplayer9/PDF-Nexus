eacb722af6dcb3b4e20171bbdb966114c66e8d3c
Om Sharma <omsharma.theplayer11@gmail.com>
2023-10-28 20:39:43 +0530

    Sing in/up working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,43 @@
 import { Button } from "@/components/ui/button";
+import { UserButton, auth } from "@clerk/nextjs";
+import Link from "next/link";
+import { LogIn } from "lucide-react";
 
-export default function Home() {
-  return <Button> naughty</Button>
+export default async function Home() {
+  const { userId } = await auth();
+  const isAuth = !!userId;
+
+  return (
+    <div className="w-screen min-h-screen bg-gradient-to-r from-blue-100 via-blue-200 to-blue-300">
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <div className="flex flex-col items-center text-center">
+          <div className="flex items-center">
+            <h1 className="mr-3 text-5xl font-semibold">Chat with any PDF</h1>
+            <UserButton afterSignOutUrl="/" />
+          </div>
+          <div className="flex mt-2">
+            {isAuth && <Button> Go to Chats</Button>}
+          </div>
+
+          <p className="max-w-xl mt-1 text-lg text-slate-600">
+            Join Millions of students, researchers and professionals ot
+            instantly answer quwstions and understand research with AI
+          </p>
+
+          <div className="w-full mt-4">
+            {isAuth ? (
+              <h1>File Upload</h1>
+            ) : (
+              <Link href="/sign-in">
+                <Button>
+                  {" "}
+                  Login to get Started! <LogIn className="w-4 h-4 ml-2"/>
+                </Button>
+              </Link>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
